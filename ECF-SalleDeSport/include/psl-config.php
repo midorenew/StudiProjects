88fcf3b3493fba31bb12c8f57e5314600578a539
midorenew <midorenew@hotmail.com>
2022-10-31 17:30:55 +0100

    Added boot folder including css/font/js subfolders

---
+++ ECF-SalleDeSport/include/psl-config.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Database login
+ */  
+define("HOST", "localhost");    // Host you want to connect to.
+define("USER", "root");         // Username. 
+define("PASSWORD", "");         // Password. 
+define("DATABASE", "sds");      // Database name.
+ 
+define("CAN_REGISTER", "any");
+define("DEFAULT_ROLE", "member");
+ 
+define("SECURE", FALSE);    // FOR DEVELOPMENT ONLY!!!!
+?>
